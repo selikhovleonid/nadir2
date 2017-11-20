3bc569ea15789a46666da2d1e10ac702b537c2c9
coon <selikhovleonid@ya.ru>
2017-11-20 14:56:09 +0300

    The AbstractCliCtrl was completely rewritten, phpDoc was added. Task #1 - Migrate Nadir microframework project from php 5.3 to php 7.1.

--- src/core/AbstractCliCtrl.php
+++ src/core/AbstractCliCtrl.php
@@ -4,67 +4,149 @@ namespace nadir2\core;
 
 /**
  * This is an abstract class of cli-controller.
- * @author Roman Lapotchkin
  * @author Leonid Selikhov
  */
 class AbstractCliCtrl extends AbstractCtrl
 {
+    /** This map consists of predefined font colors.*/
+    protected const FONT_COLOR_MAP = [
+        'black'      => '0;30',
+        'lightRed'   => '1;31',
+        'red'        => '0;31',
+        'yellow'     => '1;33',
+        'lightGreen' => '1;32',
+        'green'      => '0;32',
+        'lightBlue'  => '1;34',
+        'blue'       => '0;34',
+        'white'      => '1;37',
+    ];
+    
+    /** This map consists of predefined background colors.*/
+    protected const BG_COLOR_MAP   = [
+        'black'  => '40',
+        'red'    => '41',
+        'yellow' => '43',
+        'green'  => '42',
+        'blue'   => '44',
+    ];
 
     /**
      * The method returns options from command line argument list.
-     * @param array $aOpt An array of short options (for ex.  -f)
-     * @param array $aLongOpt An array of long options (for ex.  --foo)
-     * @return array|false It returns FALSE if an error occures.
+     * @param array $opt An array of short options (for ex.  -f)
+     * @param array $longOpt An array of long options (for ex.  --foo)
+     * @return array|false It returns false if an error occures.
      */
-    protected function getCmdOpt(array $aOpt, array $aLongOpt = array())
+    protected function getCmdOpt(array $opt, array $longOpt = [])
     {
-        return getopt(implode('', $aOpt), $aLongOpt);
+        return getopt(implode('', $opt), $longOpt);
     }
 
     /**
-     * Print message to console
-     * @param string $sMessage Message to print
-     * @return string Message with time
+     * It returns console colored message.
+     * @param string $msg The input string.
+     * @param string|null $fontColor The passed font color.
+     * @param string|null $bgColor The passed background color.
+     * @return string
      */
-    protected function printLog($sMessage)
+    private function getColoredMsg(
+        string $msg,
+        ?string $fontColor,
+        ?string $bgColor
+    ): string {
+        $res = '';
+        if (!is_null($fontColor)) {
+            $res .= "\033[{$fontColor}m";
+        }
+        if (!is_null($bgColor)) {
+            $res .= "\033[{$bgColor}m";
+        }
+        if (!is_null($fontColor) || !is_null($bgColor)) {
+            return "{$res}{$msg}\033[0m";
+        }
+        return $msg;
+    }
+
+    /**
+     * It prints passed message to the console.
+     * @param string $msg Message to print.
+     * @param bool $withTime Flag to print with previous timestamp.
+     * @return void
+     */
+    protected function print(string $msg, bool $withTime = true): void
     {
-        $oDate = new \DateTime();
-        echo $oDate->format('H:i:s')."\t{$sMessage}".PHP_EOL;
-        return '('.$oDate->format('H:i:s').') '.preg_replace(
-            '#(\[[0-9]+m)#',
-            '',
-            $sMessage
+        $preMsg = '';
+        if ($withTime) {
+            $preMsg = (new \DateTime('now'))->format('H:i:s')."\t";
+        }
+        echo "{$preMsg}{$msg}".\PHP_EOL;
+    }
+
+    /**
+     * Method prints passed error to the console.
+     * @param \Throwable $error Throwable error or exception.
+     * @param bool $withTime Flag to print with previous timestamp.
+     * @param string|null $fontColor The font color.
+     * @param string|null $bgColor The background color.
+     * @return void
+     */
+    protected function printError(
+        \Throwable $error,
+        bool $withTime = true,
+        ?string $fontColor = null,
+        ?string $bgColor = self::BG_COLOR_MAP['red']
+    ): void {
+        $shift = $withTime ? "\t\t" : '';
+        $this->print(
+            $this->getColoredMsg(
+                'Error: '.$error->getMessage(),
+                $fontColor,
+                $bgColor
+            ).\PHP_EOL
+            .$shift.$this->getColoredMsg(
+                'File: '.$error->getFile(),
+                $fontColor,
+                $bgColor
+            ).\PHP_EOL
+            .$shift.$this->getColoredMsg(
+                'Line: '.$error->getLine(),
+                $fontColor,
+                $bgColor
+            ),
+            $withTime
         );
     }
 
     /**
-     * Print error to console
-     * @param \Exception $e
-     * @return string Message with time
+     * The method prints important information to the console.
+     * @param type $msg Message to print.
+     * @param bool $withTime Flag to print with previous timestamp.
+     * @param string|null $fontColor The font color.
+     * @param string|null $bgColor The background color.
+     * @return void
      */
-    protected function printError(\Exception $e)
-    {
-        return $this->printLog("\033[41mError: {$e->getMessage()}\033[0m\n".
-                "\t\t\033[41mFile: {$e->getFile()}:{$e->getLine()}\033[0m"); //Red
+    protected function printInfo(
+        $msg,
+        bool $withTime = true,
+        ?string $fontColor = self::FONT_COLOR_MAP['lightGreen'],
+        ?string $bgColor = null
+    ): void {
+        $this->print($this->getColoredMsg($msg, $fontColor, $bgColor), $withTime);
     }
 
     /**
-     * Print important information to console
-     * @param string $sMessage Message to print
-     * @return string Message with time
+     * It prints warning message to the console.
+     * @param type $msg Message to print.
+     * @param bool $withTime Flag to print with previous timestamp.
+     * @param string|null $fontColor The font color.
+     * @param string|null $bgColor The background color.
+     * @return void
      */
-    protected function printInfo($sMessage)
-    {
-        return $this->printLog("\033[46m".$sMessage."\033[0m"); //Blue
-    }
-
-    /**
-     * Print warning to console
-     * @param string $sMessage Message to print
-     * @return string Message with time
-     */
-    protected function printWarning($sMessage)
-    {
-        return $this->printLog("\033[43m".$sMessage."\033[0m"); //Yellow
+    protected function printWarning(
+        $msg,
+        bool $withTime = true,
+        ?string $fontColor = self::FONT_COLOR_MAP['yellow'],
+        ?string $bgColor = null
+    ): void {
+        $this->print($this->getColoredMsg($msg, $fontColor, $bgColor), $withTime);
     }
 }
