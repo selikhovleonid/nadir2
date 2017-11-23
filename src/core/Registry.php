c4e0a07a29b4d2e43859fd5755e3624f18efce8e
coon <selikhovleonid@ya.ru>
2017-11-23 22:26:03 +0300

    The Registry class has been updated and extended. Task #1 - Migrate Nadir microframework project from php 5.3 to php 7.1.

--- src/core/Registry.php
+++ src/core/Registry.php
@@ -14,7 +14,7 @@ class Registry
     private static $instance = null;
 
     /** @var mixed[] The user's variable storage. */
-    protected $store = array();
+    protected $store = [];
 
     /**
      * It returns the singleton-instance of current class.
@@ -30,27 +30,36 @@ class Registry
 
     /**
      * It adds the user variable to the storage.
-     * @param string $sKey The variable name.
-     * @param mixed $mValue The variable value.
+     * @param string $key The variable name.
+     * @param mixed $value The variable value.
      * @return self.
      */
-    public function set($sKey, $mValue)
+    public function set(string $key, $value): self
     {
-        $this->store[$sKey] = $mValue;
+        $this->store[$key] = $value;
         return self::$instance;
     }
 
     /**
      * It returns the variable value getted by the name from the storage.
-     * @param string $sKey The variable name.
+     * @param string $key The variable name.
      * @return mixed|null.
      */
-    public function get($sKey = '')
+    public function get(string $key = '')
     {
-        if (empty($sKey)) {
+        if (empty($key)) {
             return $this->store;
         } else {
-            return isset($this->store[$sKey]) ? $this->store[$sKey] : null;
+            return $this->store[$key] ?? null;
         }
+    }
+
+    /**
+     * It returns the whole registry store.
+     * @return array
+     */
+    public function getAll(): array
+    {
+        return $this->get();
     }
 }
